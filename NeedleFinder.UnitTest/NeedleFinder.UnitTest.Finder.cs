2ef0e9c236312e189fb2f9cfece6ad8bef29324c
Giovanni Ferron <gvnnrulez@gmail.com>
2014-07-10 20:47:32 +1000

    Create main solution and unit tests
    Created main solution and added unit tests for the utility methods for
    string matching

---
+++ NeedleFinder.UnitTest/NeedleFinder.UnitTest.Finder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NeedleFinder.UnitTest
+{
+    [TestClass]
+    public class NeedleFinderApplication
+    {
+        [TestMethod]
+        public void TestFinder()
+        {
+            string text = "Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea";
+            
+            string result = NeedleFinder.Finder.GetMatchResult(text, "X", true);
+            Assert.AreEqual("no matches", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "Polly", true);
+            Assert.AreEqual("1, 26, 51", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "polly", true);
+            Assert.AreEqual("1, 26, 51", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "Polly", false);
+            Assert.AreEqual("1", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "polly", false);
+            Assert.AreEqual("26, 51", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, string.Empty, false);
+            Assert.AreEqual("no matches", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "PolX", false);
+            Assert.AreEqual("no matches", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "ll", true);
+            Assert.AreEqual("3, 28, 53, 78, 82", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "Ll", true);
+            Assert.AreEqual("3, 28, 53, 78, 82", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "ll", false);
+            Assert.AreEqual("3, 28, 53, 78, 82", result);
+
+            result = NeedleFinder.Finder.GetMatchResult(text, "Ll", false);
+            Assert.AreEqual("no matches", result);
+        }
+    }
+}
---
+++ NeedleFinder.UnitTest/NeedleFinder.UnitTest.Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeedleFinder.Utils;
+
+namespace NeedleFinder.UnitTest
+{
+    [TestClass]
+    public class NeedleFinderUtils
+    {
+        [TestMethod]
+        public void TestMatch()
+        {
+            string text = "Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea";
+
+            int[] matches = text.Match("Polly", true);
+            CollectionAssert.AreEqual(new int[] { 1, 26, 51 }, matches);
+
+            matches = text.Match("polly", true);
+            CollectionAssert.AreEqual(new int[] { 1, 26, 51 }, matches);
+
+            matches = text.Match("Polly");
+            CollectionAssert.AreEqual(new int[] { 1 }, matches);
+
+            matches = text.Match("polly");
+            CollectionAssert.AreEqual(new int[] { 26, 51 }, matches);
+
+            matches = text.Match(string.Empty);
+            CollectionAssert.AreEqual(new int[] { }, matches);
+
+            matches = text.Match("X");
+            CollectionAssert.AreEqual(new int[] { }, matches);
+
+            matches = text.Match("PolX");
+            CollectionAssert.AreEqual(new int[] { }, matches);
+
+            matches = text.Match("ll", true);
+            CollectionAssert.AreEqual(new int[] { 3, 28, 53, 78, 82 }, matches);
+
+            matches = text.Match("Ll", true);
+            CollectionAssert.AreEqual(new int[] { 3, 28, 53, 78, 82 }, matches);
+
+            matches = text.Match("ll");
+            CollectionAssert.AreEqual(new int[] { 3, 28, 53, 78, 82 }, matches);
+
+            matches = text.Match("Ll");
+            CollectionAssert.AreEqual(new int[] { }, matches);
+        }
+    }
+}
